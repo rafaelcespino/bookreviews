d44a06b918d0e84a3ce4fce219c4d2ee21b95968
Rafael <rafaelespino200@gmail.com>
2020-06-12 22:34:44 -0500

    Added hashing to make passwords more secure

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 # Check for environment variable
@@ -22,9 +23,6 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    books = db.execute("SELECT * FROM books").fetchall()
-    users = db.execute("SELECT * FROM users").fetchall()
-    reviews = db.execute("SELECT * FROM reviews").fetchall()
     return render_template("index.html")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -33,10 +31,34 @@ def register():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
+        confirm = request.form.get("confirm")
         name = request.form.get("name")
 
+
+        #Check to see if username is already registered
+        users = db.execute("SELECT * FROM users WHERE username = :username", {"username": username})
+        user = users.fetchone()
+        if user:
+            return render_template("invalid.html", error_message="That username already exists!", redirect="/register")
+        
+        #Check to see if any fields were left empty
+        elif not request.form.get("username"):
+            return render_template("invalid.html", error_message="Please enter a username", redirect="/register")
+
+        elif not request.form.get("password"):
+            return render_template("invalid.html", error_message="Please enter a password", redirect="/register")
+
+        elif not request.form.get("confirm"):
+            return render_template("invalid.html", error_message="Please confirm your password", redirect="/register")
+
+        #Checks to see if password matches confirmation
+        elif password != confirm:
+            return render_template("invalid.html", error_message="Passwords do not match", redirect="/register")
+
+        #Adds new user to database
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
         db.execute("INSERT INTO users (username, password, name) VALUES (:username, :password, :name)",
-                    {"username": username, "password": password, "name": name})
+                    {"username": username, "password": hashed_password, "name": name})
         db.commit()
         return render_template("success.html")
     
@@ -53,11 +75,13 @@ def login():
         users = db.execute("SELECT * FROM users WHERE username = :username", {"username": username})
         user = users.fetchone()
 
-        if user == None or user[1] != password:
-            return render_template("invalid.html")
+        if user == None or not check_password_hash(user[2], password):
+            return render_template("invalid.html", error_message="Invalid username or password", redirect="/login")
         else:
             return render_template("success.html")
 
 
     elif request.method == "GET":
         return render_template("login.html")
+
+    
